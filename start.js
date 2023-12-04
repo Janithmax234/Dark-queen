8af779ef8ab7435ee9a2a139fb759f306d9d8d38
Janith_idunil <109205729+Janithmax234@users.noreply.github.com>
2023-12-05 01:31:56 +0530

    Create start.js

---
+++ start.js
@@ -0,0 +1,22 @@
+/*
+                                ╭━━━┳╮ ╭┳━━━┳━━━┳━╮ ╭╮
+                                ┃╭━╮┃┃ ┃┃╭━━┫╭━━┫┃╰╮┃┃
+                                ┃┃ ┃┃┃ ┃┃╰━━┫╰━━┫╭╮╰╯┃
+                                ┃┃ ┃┃┃ ┃┃╭━━┫╭━━┫┃╰╮┃┃
+                                ┃╰━╯┃╰━╯┃╰━━┫╰━━┫┃ ┃┃┃
+                                ╰━━╮┣━━━┻━━━┻━━━┻╯ ╰━╯
+                                   ╰╯
+## Project Name => Dark-queen
+## Project Version => 1.0.1  
+## Project Author => Janithmax234
+## Project license => MIT
+## Project owner => Dark janeth
+## Module Name => Start.js
+## Thank For Janithmax234
+*/ 
+
+var forever = require("forever-monitor");
+var child = new forever.Monitor("Connect/module/cilent.js", {
+    silent: false,
+    });
+child.start();
